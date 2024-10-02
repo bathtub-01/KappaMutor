d83b43ae8cc18ba68dfa817c02892d9b88fb3389
BathTub <yorkgank@gmail.com>
2024-10-02 22:07:18 +0100

    dev env setup

---
+++ src/main/scala/maker.scala
@@ -0,0 +1,10 @@
+package maker
+
+import chisel3._
+import circt.stage.ChiselStage
+
+import gcd._
+
+object VerilogMain extends App {
+  ChiselStage.emitSystemVerilogFile(new GCD, Array("--target-dir", "generated"))
+}
