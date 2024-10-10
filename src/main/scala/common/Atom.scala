b5b1d9fc76e81889338dd344c45e9c3c5dfa6ef1
BathTub <yorkgank@gmail.com>
2024-10-10 10:14:54 +0100

    add: SystemConfig and Atom definition

---
+++ src/main/scala/common/Atom.scala
@@ -0,0 +1,40 @@
+package common
+
+import chisel3._
+import chisel3.util._
+
+import SystemConfig._
+
+/*  
+  FUN: function pointer
+  PTR: heap pointer
+  COM: structured combinator
+*/
+object AtomType extends ChiselEnum {
+  val FUN = Value
+  val PTR = Value
+  val COM = Value
+}
+
+abstract class AtomPayload extends Bundle
+
+/*  
+  Payload for FUN and PTR, which only contains a pointer
+*/
+class AddrPayload extends AtomPayload {
+  val pointer = UInt(atomPayloadSize.W)
+}
+
+/*  
+  Payload for COM, with arity, pattern and an index vector
+*/
+class ComPayload extends AtomPayload {
+  val arity = UInt(log2Ceil(comArity + 1).W)
+  val pattern = UInt(log2Ceil(comPattern).W)
+  val idxs = Vec(comIdxs, UInt(log2Ceil(comArity + 1).W))
+}
+
+class Atom extends Bundle {
+  val atomType = AtomType()
+  val payload = Bits(atomPayloadSize.W)
+}
---
+++ src/main/scala/common/SystemConfig.scala
@@ -0,0 +1,20 @@
+package common
+
+import chisel3._
+import chisel3.util._
+
+/* 
+  atomSize: size of an Atom
+  stackN: XRegStack
+  comArity: maximum arity for structured combinator
+  comPatter: patterns supported by structured combinator
+  comIdxs: maximum de Brujin indexies in structured combinator
+*/
+object SystemConfig {
+  val atomSize = 32
+  val stackN = 8
+  val comArity = 6
+  val comPattern = 64
+  val comIdxs = 6
+  def atomPayloadSize = atomSize - AtomType.getWidth
+}
